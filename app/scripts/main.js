4a10e6d537f8f2caeafc1f5026876e57df97d9d5
Lukasz Grzegorz Maciak <maciakl@gmail.com>
2014-05-15 04:12:04 +0100

    Layout turd-polishing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,42 +1,71 @@
-$.getJSON("http://www.reddit.com/user/reset_by_peer/m/imaginary/.json?jsonp=?&show=all&limit=300", function(data) {
-    $.each(data.data.children, function(i,item){
-        IsValidImageUrl(item.data.url, function(url, isvalid) {
-            if(isvalid) { 
-                $("<img/>").attr("src", item.data.url)
-                .addClass("img-responsive").addClass("img-rounded")
-                .appendTo(
-                    $("<a />").attr("href", item.data.url)
-                    .addClass("thumbnail")
-                    .appendTo(
-                        $("<div />").addClass("col-xs-12")
-                            .appendTo("#images")
-                    )
-                )
-            }
-            else {
-                if(!item.data.is_self)
-                {
-                    $("<img/>").attr("src", item.data.thumbnail)
-                    .addClass("img-thumbnail")
-                    .appendTo(
-                        $("<a />").attr("href", item.data.url)
-                        .addClass("thumbnail")
-                        .appendTo(
-                            $("<div />").addClass("col-xs-12 col-sm-6 col-md-4")
-                                .appendTo("#images")
-                        )
-                    );
-                }
-            }
-            
-        });
-    });
-});
+'use strict';
 
+var LIMIT = 500;
 
 function IsValidImageUrl(url, callback) {
     var img = new Image();
-    img.onerror = function() { callback(url, false); }
-    img.onload =  function() { callback(url, true); }
-    img.src = url
+    img.onerror = function() { callback(false); };
+    img.onload =  function() { callback(true); };
+    img.src = url;
 }
+
+$.getJSON('http://www.reddit.com/user/reset_by_peer/m/imaginary/.json?jsonp=?&show=all&limit='+LIMIT, function(data) {
+    $.each(data.data.children, function(i,item){
+
+        new IsValidImageUrl(item.data.url, function(isvalid) {
+
+            if(isvalid) {
+
+                $('#images').append(
+                    $('<div />').addClass('col-xs-12')
+                    .append(
+                        $('<div />').addClass('thumbnail')
+                        .append(
+                            $('<img />').attr('src', item.data.url)
+                            .attr('alt', item.data.title)
+                            .addClass('img-responsive img-rounded')
+                        ).append(
+                            $('<div />').addClass('caption')
+                            .append($('<p />').addClass('text-muted').append(item.data.title))
+                            .append($('<small />')
+                                .append(
+                                    $('<a />').attr('href', 'http://reddit.com'+item.data.permalink)
+                                    .addClass('btn btn-default btn-xs')
+                                    .append($('<span />').addClass('fa fa-reddit'))
+                                    .append(' ' + item.data.subreddit)
+                                )
+                                .append(' ')
+                                .append(
+                                    $('<span />')
+                                    .addClass('label label-warning')
+                                    .append($('<span />').addClass('fa fa-arrow-up'))
+                                    .append(' ' + item.data.ups)
+                                )
+                                .append(
+                                    $('<span />')
+                                    .addClass('label label-info')
+                                    .append($('<span />').addClass('fa fa-arrow-down'))
+                                    .append(' ' + item.data.downs)
+                                )
+                                .append(' ')
+                                .append(
+                                    $('<span />')
+                                    .addClass('label label-default')
+                                    .append($('<span />').addClass('fa fa-user'))
+                                    .append(' ' + item.data.author)
+                                )
+                                .append(' ')
+                                .append(
+                                    $('<a />').attr('href', item.data.url)
+                                    .addClass('btn btn-default btn-xs')
+                                    .append($('<span />').addClass('fa fa-image'))
+                                    .append(' ' + item.data.domain)
+                                )
+                            )
+                        )
+                    )
+                );
+            }
+        });
+    });
+});
